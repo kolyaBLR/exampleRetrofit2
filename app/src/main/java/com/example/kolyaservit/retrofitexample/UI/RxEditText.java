744d5bc449bd1c5ecfcfeff643c90f6ded5e2480
Nikolay Bobrov <bk97w@bk.ru>
2018-02-10 20:12:35 +0300

    add rx search address

---
+++ app/src/main/java/com/example/kolyaservit/retrofitexample/UI/RxEditText.java
@@ -0,0 +1,28 @@
+package com.example.kolyaservit.retrofitexample.UI;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import io.reactivex.Observable;
+import io.reactivex.internal.operators.observable.ObservableDebounce;
+import io.reactivex.subjects.PublishSubject;
+
+public class RxEditText {
+    public static Observable<String> addTextWatcher(EditText editor) {
+        PublishSubject<String> subject = PublishSubject.create();
+        editor.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                subject.onNext(s.toString());
+            }
+        });
+        return subject;
+    }
+}
--- app/src/main/java/com/example/kolyaservit/retrofitexample/UI/SearchAddressFragment.java
+++ app/src/main/java/com/example/kolyaservit/retrofitexample/UI/SearchAddressFragment.java
@@ -2,7 +2,8 @@ package com.example.kolyaservit.retrofitexample.UI;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +14,8 @@ import android.widget.TextView;
 import com.example.kolyaservit.retrofitexample.R;
 import com.example.kolyaservit.retrofitexample.Retrofit.Google.GoogleAPI;
 import com.example.kolyaservit.retrofitexample.Retrofit.Google.data.GoogleAddress;
-import com.example.kolyaservit.retrofitexample.UI.MainActivity;
-import com.google.android.gms.maps.MapView;
+
+import java.util.concurrent.TimeUnit;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,31 +24,19 @@ import retrofit2.Response;
 public class SearchAddressFragment extends Fragment {
 
     private EditText searchText;
-    private Button searchButton;
     private TextView resultSearch;
-    private MapView mapView;
-
-    private final String RESULT_SEARCH_TAG = "SearchAddressFragment_resultSearch";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_search_address, container, false);
         searchText = v.findViewById(R.id.search_edit_text);
-        searchButton = v.findViewById(R.id.search_button);
         resultSearch = v.findViewById(R.id.result_text_view);
-        mapView = v.findViewById(R.id.map_address);
 
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String address = searchText.getText().toString();
-                if (!address.isEmpty()) {
-                    GoogleAPI.getApi().searchAddress(address).enqueue(responseGoogle);
-                }
-            }
-        });
-
+        RxEditText.addTextWatcher(searchText)
+                .debounce(750, TimeUnit.MILLISECONDS)
+                .filter(s -> !s.isEmpty())
+                .subscribe(s -> GoogleAPI.getApi().searchAddress(s).enqueue(responseGoogle));
         return v;
     }
 
